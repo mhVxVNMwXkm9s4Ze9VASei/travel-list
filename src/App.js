349453c775811aa4628e1e1739c743624b6f3f88
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-07-24 23:02:53 -0500

    Item Deletion
    :0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-	{ id: 1, description: "Passports", quantity: 2, packed: false },
-	{ id: 2, description: "Socks", quantity: 12, packed: false },
-	{ id: 3, description: "Charger", quantity: 1, packed: true },
-];
-
 export default function App() {
 	const [items, setItems] = useState([]);
 
@@ -13,11 +7,18 @@ export default function App() {
 		setItems((items) => [...items, item]);
 	}
 
+	function handleDeleteItem(id) {
+		setItems((items) => items.filter((item) => item.id !== id));
+	}
+
 	return (
 		<div className="app">
 			<Logo />
 			<Form onAddItems={handleAddItems} />
-			<PackingList items={items} />
+			<PackingList
+				items={items}
+				onDeleteItem={handleDeleteItem}
+			/>
 			<Stats />
 		</div>
 	);
@@ -69,7 +70,7 @@ function Form({ onAddItems }) {
 	);
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
 	return (
 		<div className="list">
 			<ul>
@@ -77,6 +78,7 @@ function PackingList({ items }) {
 					<Item
 						key={item.id}
 						item={item}
+						onDeleteItem={onDeleteItem}
 					/>
 				))}
 			</ul>
@@ -84,13 +86,13 @@ function PackingList({ items }) {
 	);
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
 	return (
 		<li>
 			<span style={item.packed ? { textDecoration: "line-through" } : {}}>
 				{item.quantity} {item.description}
 			</span>
-			<button>❌</button>
+			<button onClick={() => onDeleteItem(item.id)}>❌</button>
 		</li>
 	);
 }
