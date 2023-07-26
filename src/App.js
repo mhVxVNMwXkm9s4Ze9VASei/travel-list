cc8d5395cca89de7432d7989bd7037642fcb6c40
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-07-25 22:01:21 -0500

    Updated Stats
    Used items to create derived state.

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
 				onDeleteItem={handleDeleteItem}
 				onToggleItem={handleToggleItem}
 			/>
-			<Stats />
+			<Stats items={items} />
 		</div>
 	);
 }
@@ -112,10 +112,27 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 	);
 }
 
-function Stats() {
+function Stats({ items }) {
+	if (!items.length)
+		return (
+			<footer className="stats">
+				<em>Start adding some items to your packing list.</em>
+			</footer>
+		);
+
+	const numItems = items.length;
+	const numPacked = items.filter((item) => item.packed).length;
+	const percentage = Math.round((numPacked / numItems) * 100);
+
 	return (
 		<footer className="stats">
-			<em>💼 You have X items on your list, and you already packed X (X%)</em>.
+			<em>
+				{percentage === 100
+					? "You got everything. Ready to go! ✈"
+					: `💼 You have ${numItems} item${
+							numItems > 1 ? "s" : ""
+					  } on your list, and you already packed ${numPacked} (${percentage}%).`}
+			</em>
 		</footer>
 	);
 }
